ac441528218cc8069e8c21c908d19be6f34b3e82
dotz0ver <opir0oui@gmail.com>
2024-12-23 12:19:22 +0900

    Chore: Add Dockerfile and Docker Compose configurations for app and database

---
+++ Backend/mini7/src/main/java/com/aivle/mini7/controller/LoadModelController.java
@@ -0,0 +1,4 @@
+package com.aivle.mini7.controller;
+
+public class LoadModelController {
+}
---
+++ Backend/mini7/src/main/java/com/aivle/mini7/service/LoadModelService.java
@@ -0,0 +1,6 @@
+package com.aivle.mini7.service;
+
+import java.util.Map;
+
+public class LoadModelService {
+}
