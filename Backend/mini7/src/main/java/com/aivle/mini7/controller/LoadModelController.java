7fe53b600742d3e9d3a9e9be2068de7518b52ee8
dotz0ver <opir0oui@gmail.com>
2024-12-23 14:37:28 +0900

    Feat: Implement load emergency model API

--- Backend/mini7/src/main/java/com/aivle/mini7/controller/LoadModelController.java
+++ Backend/mini7/src/main/java/com/aivle/mini7/controller/LoadModelController.java
@@ -1,4 +1,24 @@
 package com.aivle.mini7.controller;
 
+import com.aivle.mini7.service.LoadModelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
 public class LoadModelController {
+
+    @Autowired
+    private LoadModelService loadModelService;
+
+    @GetMapping("/hospital/load_emergency_model")
+    public Map<String, Object> loadModel() {
+        try {
+            return loadModelService.loadEmergencyModel();
+        } catch (Exception e) {
+            return Map.of("status", "error", "message", e.getMessage());
+        }
+    }
 }
