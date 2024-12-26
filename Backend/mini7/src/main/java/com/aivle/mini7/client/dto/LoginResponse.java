86f1d37954eb42a73a7e4fb4e8265505ae2d72f9
dotz0ver <opir0oui@gmail.com>
2024-12-26 10:49:25 +0900

    Feat: Implement user login API

---
+++ Backend/mini7/src/main/java/com/aivle/mini7/client/dto/LoginResponse.java
@@ -0,0 +1,12 @@
+package com.aivle.mini7.client.dto;
+
+public class LoginResponse {
+
+    private String message;
+
+    public LoginResponse(String message) { this.message = message; }
+
+    public String getMessage() { return message; }
+
+    public void setMessage(String message) { this.message = message; }
+}
--- Backend/mini7/src/main/java/com/aivle/mini7/service/UserService.java
+++ Backend/mini7/src/main/java/com/aivle/mini7/service/UserService.java
@@ -1,5 +1,6 @@
 package com.aivle.mini7.service;
 
+import com.aivle.mini7.client.dto.LoginResponse;
 import com.aivle.mini7.client.dto.RegisterRequest;
 import com.aivle.mini7.entity.User;
 import com.aivle.mini7.repository.UserRepository;
@@ -36,4 +37,18 @@ public class UserService {
 
         userRepository.save(user);
     }
+
+    public LoginResponse loginUser(String userId, String password) {
+        // 사용자 확인
+        User user = userRepository.findByUserId(userId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 ID입니다."));
+
+        // 비밀번호 확인
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 틀렸습니다.");
+        }
+
+        return new LoginResponse("로그인 성공");
+    }
+
 }
